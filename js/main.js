6382d8095043183e17d02cc1c8b89532b81c1aad
spoleschi <68704211+spoleschi@users.noreply.github.com>
2022-11-14 17:14:07 -0300

    Validación en producto y cant = 0 e informo total solo si compro

--- js/main.js
+++ js/main.js
@@ -1,21 +1,24 @@
-
+//Defino e inicializo variables
 let rta, producto, totalcompras, pedido;
 rta = true;
 totalcompras = 0;
 pedido = 'Pedido realizado:  \n \n'
 
-
+//Creo la clase productos para almacenar los distintos modelos de sillas
 class Producto {
     constructor(id, nombre, precio){
         this.id = id;
         this.nombre = nombre;
         this.precio = precio;
     }
+
+    //función de clase (se genera una vez por prototipo)
     listate (){
         return `Código de silla : ${this.id} - Modelo : ${this.nombre} - Precio : $ ${this.precio} \n`;
     }
-    
 }
+
+//Genero instancias de la clase producto con los distintos modelos de silla
 
 const silla01 = new Producto(1,'wishbone',34000)
 const silla02 = new Producto(2,'kennedy',29000)
@@ -24,15 +27,19 @@ const silla04 = new Producto(4,'ch20',25000)
 const silla05 = new Producto(5,'grace',39000)
 const silla06 = new Producto(6,'febo',24000)
 
+//Creo un array con los modelos de silla
 const sillas = [silla01, silla02,silla03, silla04,silla05, silla06]
 
+//Creo una función que muestra las opciones al usuario
 function mostrarProductos(){
     let productos = 'Listado de Productos: \n';
     
+    //Utilizando forEach
     // sillas.forEach(function(element) {
     //      productos = productos + `\n Código de silla : ${element.id} - Modelo : ${element.nombre} - Precio : ${element.precio} `;
     // });
 
+    //Utilizando for of
     for (const silla of sillas){
         //productos = productos + `\n Código de silla : ${silla.id} - Modelo : ${silla.nombre} - Precio : ${silla.precio} `;
         productos = productos + ' ' + silla.listate();
@@ -41,12 +48,15 @@ function mostrarProductos(){
     window.alert(productos);
 }
 
+//Inicio de programa
+
 rta = window.confirm('¿Desea realizar una compra?');
 
+//Ciclo para que el usuario ingrese los productos y cantidad deseada
 while (rta == true){
     mostrarProductos();
     producto = Number(prompt('Ingrese el nro. de modelo de silla que quiere comprar'));
-    let cant = prompt('Ingresa la cantidad deseada');
+    let cant = Number(prompt('Ingresa la cantidad deseada'));
 
     // switch (producto){
     //     case 1:
@@ -71,9 +81,10 @@ while (rta == true){
     //         window.alert('Debe ingresar un modelo válido de silla(del 1 al 6)')
     // }
 
-//Utilizo un if accediendo al precio por la posición en el array...
-    if (isNaN(producto) == false && producto < 7){
-        if (isNaN(cant) == false) {
+//Utilizo un if accediendo al precio por la posición en el array para no recorrer todas las opciones en un switch...
+    
+if (isNaN(producto) == false && producto > 0 && producto < 7){
+        if (isNaN(cant) == false && cant > 0) {
             totalcompras += sillas[Number(producto)-1].precio * cant;
             pedido = pedido + cant + ' ' +  sillas[Number(producto)-1].nombre + ' \n'
             window.alert(`Agregamos al carrito ${cant} unidad/es de la silla: ${sillas[Number(producto)-1].nombre}`);
@@ -88,5 +99,10 @@ while (rta == true){
 
     rta = window.confirm('¿Desea seleccionar otro pruducto?');
 }
-window.alert(pedido);
-window.alert(`El monto total de la compra es de $ ${totalcompras}`);
+
+//Si se ha realizado la compra informo el pedido y el total de la operación
+if (totalcompras){
+    window.alert(pedido);
+    window.alert(`El monto total de la compra es de $ ${totalcompras}`);
+}
+
