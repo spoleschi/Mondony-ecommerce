1850bf5d1dba7e1e09917cfe0110dd6ec5a2b03f
spoleschi <spoleschi@users.noreply.github.com>
2022-11-29 23:09:40 -0300

    Cards dinámicas con js

--- js/main.js
+++ js/main.js
@@ -5,10 +5,11 @@ const arrayPedido = [];
 //Clases
 
 class Producto {
-    constructor(id, nombre, precio){
+    constructor(id, nombre, precio, imagen){
         this.id = id;
         this.nombre = nombre;
         this.precio = precio;
+        this.imagen = imagen;
         this.stock = 0;
     }
 
@@ -116,21 +117,37 @@ function buscarIdx(valBus){
     return indice
 }
 
-
-
 //Genero instancias de la clase producto con los distintos modelos de silla
 
-const silla01 = new Producto(1,'wishbone',34000);
-const silla02 = new Producto(2,'kennedy',29000);
-const silla03 = new Producto(3,'moller',23000);
-const silla04 = new Producto(4,'ch20',25000);
-const silla05 = new Producto(5,'grace',39000);
-const silla06 = new Producto(6,'febo',24000);
+const silla01 = new Producto(1,'wishbone',34000,'./images/Sillas/wishbone.jpg');
+const silla02 = new Producto(2,'kennedy',29000,'./images/Sillas/kennedy.jpg');
+const silla03 = new Producto(3,'moller',23000,'./images/Sillas/moller.jpg');
+const silla04 = new Producto(4,'ch20',25000,'./images/Sillas/Ch20Elbow.jpg');
+const silla05 = new Producto(5,'grace',39000,'./images/Sillas/grace.jpg');
+const silla06 = new Producto(6,'febo',24000,'./images/Sillas/febo.jpg');
 
 //Creo un array con los modelos de silla
 const sillas = [silla01, silla02,silla03, silla04,silla05, silla06]
 
 //Main
+
+const resultado = document.getElementById("resultado");
+
+// Ver como agregar las imágenes a objetos
+
+
+for ( let silla of sillas ){
+    resultado.innerHTML += `
+    <div class="card m-3" style="width: 18rem;" id="resultado">
+        <img src= ${silla.imagen} class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Nombre: ${silla.nombre}</h5>
+            <p class="card-text">Valor: ${silla.precio}</p>
+        </div>
+    </div>
+    `
+}
+
 
 window.onload = function() { // can also use window.addEventListener('load', (event) => {
     // alert('Page loaded');
